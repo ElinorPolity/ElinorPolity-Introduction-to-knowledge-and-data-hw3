7f48c91c863e1edce1d4e7d5024c0893191e0be6
ElinorPolity <80193620+ElinorPolity@users.noreply.github.com>
2021-05-03 11:55:59 +0300

    Update TASK3.py

--- TASK3.py
+++ TASK3.py
@@ -1,12 +1,13 @@
 import json
 import os
+# first function - get the text file, splits it into rows and uses the other function
 def Read_Messages(path:str) -> str :
     Text=open(path,encoding=("utf8"))
     Text=Text.read()
     Text=Text.split("\n")
     return Organize_In_Derectory(Text)
     
-
+#the function where we give code names to paricipens and converting the text file into json
 def Organize_In_Derectory(Text:str): 
     phoneInfo=dict()
     All_directoris=[]
@@ -15,11 +16,13 @@ def Organize_In_Derectory(Text:str):
     ID=0
     counter=0
     for line in Text: 
+        # finding the row with the information about the founder of the group
         if "הקבוצה" in line and "נוצרה על ידי" in line:
             date_Metadata=line[:line.find("-")]
             group_title=line[27:len(line)-35]
             creator=line[len(line)-21:]
             MetaData={"chat_name":group_title,"creation_data":date_Metadata,"creator":creator.strip()}
+        #finding all phone numbers or names and checking if we allready converted them to ID
         if ": " in line:
             phone_num=line[18:line.find(": ")]
             if phone_num in phoneOrName:
@@ -29,22 +32,28 @@ def Organize_In_Derectory(Text:str):
                         phoneInfo={"date":line[:line.find("-")-1],"ID":TheID,"text":line[line.find(": ")+2:].strip()}
                         All_directoris+=[phoneInfo]
                         counter+=1
+            #if we didnt convert them to ID allready then:
             else:
                 ID=ID+1
-                phoneInfo=phoneInfo={"date":line[:line.find("-")-1],"ID":str(ID),"text":line[line.find(": ")+2:].strip()}
+                phoneInfo={"date":line[:line.find("-")-1],"ID":str(ID),"text":line[line.find(": ")+2:].strip()}
                 All_directoris+=[phoneInfo]
                 phoneOrName+=[phone_num]
                 Id_list+=[str(ID)]
         
         counter=0
+    #combin the two dict to one dict, conveting to json and saving as text file/
     MetaData["num_of_participants"]=ID
     combine_derectory={"messages":All_directoris,"metadata":MetaData}
     json_file=json.dumps(combine_derectory, ensure_ascii=False, indent=5)
     print(json_file)
     with open(os.path.join('C:\hw3',group_title+".txt") , 'w',encoding='utf-8') as f:
-       f.write(json_file)      
+       f.write(json_file)
+       
+#calling the function
 Read_Messages("C:\hw3\WhatsApp.txt")
                  
+        
+       
         
                    
     
